7a8599cfa2016b46a0dca1b858cc3be8030a12fd
can101 <can896373@gmail.com>
2023-02-14 06:44:19 +0300

    feat: add input component

--- src/components/_atoms/form/input/Input.tsx
+++ src/components/_atoms/form/input/Input.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import type { FC, ReactElement } from 'react';
+import type { FC, ReactElement, ReactNode } from 'react';
 import styles from './input.module.scss';
+import Button from '../../buttons/circle-icon-button';
 
 interface IInputProps {
   type?: string;
   placeholder: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  icon?: ReactNode;
 }
 
-const BaseInput: FC<IInputProps> = ({ type = 'text', placeholder }): ReactElement => {
+const Input: FC<IInputProps> = ({ type = 'text', placeholder, icon }): ReactElement => {
   return (
     <div className={styles.input_box}>
       <input type={type} className={styles.input_box__input} placeholder={placeholder} />
+      <div className={styles.input_box__button}></div>
+      {icon && (
+        <div className={styles.input_box__icon}>
+          <Button
+            size={'sm'}
+            icon={icon}
+            onClick={function (): void {
+              throw new Error('Function not implemented.');
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
 
-export default BaseInput;
+export default Input;
