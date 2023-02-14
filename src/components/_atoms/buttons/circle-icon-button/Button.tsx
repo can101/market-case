0f9664c008fe0c06e165e26ca582aa59615283f2
can101 <can896373@gmail.com>
2023-02-14 06:43:12 +0300

    feat: add button size property

--- src/components/_atoms/buttons/circle-icon-button/Button.tsx
+++ src/components/_atoms/buttons/circle-icon-button/Button.tsx
@@ -1,37 +1,29 @@
 import React from 'react';
-import type {FC, ReactElement, ReactNode} from 'react';
+import type { FC, ReactElement, ReactNode } from 'react';
 import styles from './button.module.scss';
 
 export interface IButtonProps {
-    onClick: () => void;
-    title?: string;
-    icon?: ReactNode;
-    disabled?: boolean;
-    className?: string;
-    quanttiy?: number;
-    type?: 'button' | 'submit' | 'reset';
+  onClick: () => void;
+  title?: string;
+  icon?: ReactNode;
+  disabled?: boolean;
+  className?: string;
+  quanttiy?: number;
+  type?: 'button' | 'submit' | 'reset';
+  size?: 'xs' | 'sm' | 'md' | 'lg';
 }
 
-const Button: FC<IButtonProps> = ({
-                                      disabled = false,
-                                      onClick,
-                                      icon,
-                                      type,
-                                      title,
-                                      quanttiy,
-                                      className = styles.button
-                                  }): ReactElement => {
-    return (
-        <button type={type} disabled={disabled} className={className} onClick={onClick}>
-            <span className={styles.button__icon}>
-               {quanttiy && <div className={styles.button__badge}>{quanttiy}</div>}
-                {icon}
-            </span>
-            {title && <span className={styles.button__title}>
-                {title}
-            </span>}
-        </button>
-    );
-}
+const Button: FC<IButtonProps> = ({ disabled = false, onClick, icon, type, title, quanttiy, size = 'lg', className = styles.button }): ReactElement => {
+  const sizeClass = styles[`button__${size}`];
+  return (
+    <button type={type} disabled={disabled} className={`${styles.button} ${sizeClass}`} onClick={onClick}>
+      <span className={styles.button__icon}>
+        {quanttiy && <div className={styles.button__badge}>{quanttiy}</div>}
+        {icon}
+      </span>
+      {title && <span className={styles.button__title}>{title}</span>}
+    </button>
+  );
+};
 
 export default Button;
